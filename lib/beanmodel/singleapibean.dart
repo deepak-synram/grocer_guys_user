5ac0a2b8dbd87d7d568992773a1946c9e1c42dff
Deepeshac2890 <deepeshac290@gmail.com>
2022-03-31 14:54:25 +0530

    is_subscribe related Api integration

--- lib/beanmodel/singleapibean.dart
+++ lib/beanmodel/singleapibean.dart
@@ -2,13 +2,13 @@ class SingleApiHomePage {
   String status;
   String message;
   List<BannerDataModel> banner;
-  List<void> secondBanner;
+  List<Null> secondBanner;
   List<TopCat> topCat;
   List<Recentselling> recentselling;
   List<Topselling> topselling;
-  List<Recentselling> dealproduct;
+  List<Null> dealproduct;
   List<Whatsnew> whatsnew;
-  List<Recentselling> spotlight;
+  List<Null> spotlight;
 
   SingleApiHomePage(
       {this.status,
@@ -64,6 +64,7 @@ class SingleApiHomePage {
     if (this.banner != null) {
       data['banner'] = this.banner.map((v) => v.toJson()).toList();
     }
+
     if (this.topCat != null) {
       data['top_cat'] = this.topCat.map((v) => v.toJson()).toList();
     }
@@ -82,11 +83,11 @@ class SingleApiHomePage {
 }
 
 class BannerDataModel {
-  int bannerId;
+  num bannerId;
   String bannerName;
   String bannerImage;
-  int storeId;
-  int catId;
+  num storeId;
+  num catId;
   String type;
   String title;
 
@@ -124,12 +125,12 @@ class BannerDataModel {
 
 class TopCat {
   String title;
-  int catId;
+  num catId;
   String image;
-  int storeId;
+  num storeId;
   String description;
-  int stfrom;
-  int subcatCount;
+  num stfrom;
+  num subcatCount;
 
   TopCat(
       {this.title,
@@ -164,24 +165,25 @@ class TopCat {
 }
 
 class Recentselling {
-  int storeId;
-  int stock;
-  int varientId;
-  int productId;
+  num storeId;
+  num stock;
+  num varientId;
+  num productId;
   String productName;
   String productImage;
   String description;
-  int price;
-  int mrp;
-  Null varientImage;
+  num price;
+  num mrp;
+  String varientImage;
   String unit;
-  int quantity;
+  num quantity;
   String type;
-  int count;
+  num isSubscribe;
+  num count;
   String isFavourite;
-  int cartQty;
-  int avgrating;
-  int countrating;
+  num cartQty;
+  num avgrating;
+  num countrating;
   num discountper;
   List<Varients> varients;
   List<Images> images;
@@ -201,6 +203,7 @@ class Recentselling {
       this.unit,
       this.quantity,
       this.type,
+      this.isSubscribe,
       this.count,
       this.isFavourite,
       this.cartQty,
@@ -221,251 +224,11 @@ class Recentselling {
     description = json['description'];
     price = json['price'];
     mrp = json['mrp'];
-    // varientImage = json['varient_image'];
-    unit = json['unit'];
-    quantity = json['quantity'];
-    type = json['type'];
-    count = json['count'];
-    isFavourite = json['isFavourite'];
-    cartQty = json['cart_qty'];
-    avgrating = json['avgrating'];
-    countrating = json['countrating'];
-    discountper = json['discountper'];
-    if (json['varients'] != null) {
-      varients = new List<Varients>();
-      json['varients'].forEach((v) {
-        varients.add(new Varients.fromJson(v));
-      });
-    }
-    if (json['images'] != null) {
-      images = new List<Images>();
-      json['images'].forEach((v) {
-        images.add(new Images.fromJson(v));
-      });
-    }
-    if (json['tags'] != null) {
-      tags = new List<Tags>();
-      json['tags'].forEach((v) {
-        tags.add(new Tags.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['store_id'] = this.storeId;
-    data['stock'] = this.stock;
-    data['varient_id'] = this.varientId;
-    data['product_id'] = this.productId;
-    data['product_name'] = this.productName;
-    data['product_image'] = this.productImage;
-    data['description'] = this.description;
-    data['price'] = this.price;
-    data['mrp'] = this.mrp;
-    // data['varient_image'] = this.varientImage;
-    data['unit'] = this.unit;
-    data['quantity'] = this.quantity;
-    data['type'] = this.type;
-    data['count'] = this.count;
-    data['isFavourite'] = this.isFavourite;
-    data['cart_qty'] = this.cartQty;
-    data['avgrating'] = this.avgrating;
-    data['countrating'] = this.countrating;
-    data['discountper'] = this.discountper;
-    if (this.varients != null) {
-      data['varients'] = this.varients.map((v) => v.toJson()).toList();
-    }
-    if (this.images != null) {
-      data['images'] = this.images.map((v) => v.toJson()).toList();
-    }
-    if (this.tags != null) {
-      data['tags'] = this.tags.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
-class Varients {
-  int storeId;
-  int stock;
-  int varientId;
-  String description;
-  int price;
-  int mrp;
-  Null varientImage;
-  String unit;
-  int quantity;
-  Null dealPrice;
-  Null validFrom;
-  Null validTo;
-  String isFavourite;
-  int cartQty;
-  num avgrating;
-  int countrating;
-  num discountper;
-
-  Varients(
-      {this.storeId,
-      this.stock,
-      this.varientId,
-      this.description,
-      this.price,
-      this.mrp,
-      this.varientImage,
-      this.unit,
-      this.quantity,
-      this.dealPrice,
-      this.validFrom,
-      this.validTo,
-      this.isFavourite,
-      this.cartQty,
-      this.avgrating,
-      this.countrating,
-      this.discountper});
-
-  Varients.fromJson(Map<String, dynamic> json) {
-    storeId = json['store_id'];
-    stock = json['stock'];
-    varientId = json['varient_id'];
-    description = json['description'];
-    price = json['price'];
-    mrp = json['mrp'];
-    // varientImage = json['varient_image'];
-    unit = json['unit'];
-    quantity = json['quantity'];
-    dealPrice = json['deal_price'];
-    validFrom = json['valid_from'];
-    validTo = json['valid_to'];
-    isFavourite = json['isFavourite'];
-    cartQty = json['cart_qty'];
-    avgrating = json['avgrating'];
-    countrating = json['countrating'];
-    discountper = json['discountper'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['store_id'] = this.storeId;
-    data['stock'] = this.stock;
-    data['varient_id'] = this.varientId;
-    data['description'] = this.description;
-    data['price'] = this.price;
-    data['mrp'] = this.mrp;
-    // data['varient_image'] = this.varientImage;
-    data['unit'] = this.unit;
-    data['quantity'] = this.quantity;
-    data['deal_price'] = this.dealPrice;
-    data['valid_from'] = this.validFrom;
-    data['valid_to'] = this.validTo;
-    data['isFavourite'] = this.isFavourite;
-    data['cart_qty'] = this.cartQty;
-    data['avgrating'] = this.avgrating;
-    data['countrating'] = this.countrating;
-    data['discountper'] = this.discountper;
-    return data;
-  }
-}
-
-class Images {
-  String image;
-
-  Images({this.image});
-
-  Images.fromJson(Map<String, dynamic> json) {
-    image = json['image'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['image'] = this.image;
-    return data;
-  }
-}
-
-class Tags {
-  int tagId;
-  int productId;
-  String tag;
-
-  Tags({this.tagId, this.productId, this.tag});
-
-  Tags.fromJson(Map<String, dynamic> json) {
-    tagId = json['tag_id'];
-    productId = json['product_id'];
-    tag = json['tag'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['tag_id'] = this.tagId;
-    data['product_id'] = this.productId;
-    data['tag'] = this.tag;
-    return data;
-  }
-}
-
-class Topselling {
-  int storeId;
-  int stock;
-  int varientId;
-  int productId;
-  String productName;
-  String productImage;
-  String description;
-  int price;
-  int mrp;
-  String varientImage;
-  String unit;
-  int quantity;
-  String type;
-  int count;
-  String isFavourite;
-  int cartQty;
-  num avgrating;
-  int countrating;
-  num discountper;
-  List<Varients> varients;
-  List<Images> images;
-  List<Tags> tags;
-
-  Topselling(
-      {this.storeId,
-      this.stock,
-      this.varientId,
-      this.productId,
-      this.productName,
-      this.productImage,
-      this.description,
-      this.price,
-      this.mrp,
-      this.varientImage,
-      this.unit,
-      this.quantity,
-      this.type,
-      this.count,
-      this.isFavourite,
-      this.cartQty,
-      this.avgrating,
-      this.countrating,
-      this.discountper,
-      this.varients,
-      this.images,
-      this.tags});
-
-  Topselling.fromJson(Map<String, dynamic> json) {
-    storeId = json['store_id'];
-    stock = json['stock'];
-    varientId = json['varient_id'];
-    productId = json['product_id'];
-    productName = json['product_name'];
-    productImage = json['product_image'];
-    description = json['description'];
-    price = json['price'];
-    mrp = json['mrp'];
     varientImage = json['varient_image'];
     unit = json['unit'];
     quantity = json['quantity'];
     type = json['type'];
+    isSubscribe = json['is_subscribe'];
     count = json['count'];
     isFavourite = json['isFavourite'];
     cartQty = json['cart_qty'];
@@ -507,6 +270,7 @@ class Topselling {
     data['unit'] = this.unit;
     data['quantity'] = this.quantity;
     data['type'] = this.type;
+    data['is_subscribe'] = this.isSubscribe;
     data['count'] = this.count;
     data['isFavourite'] = this.isFavourite;
     data['cart_qty'] = this.cartQty;
@@ -526,30 +290,151 @@ class Topselling {
   }
 }
 
-class Whatsnew {
-  int storeId;
-  int stock;
-  int varientId;
-  int productId;
+class Varients {
+  num storeId;
+  num stock;
+  num varientId;
+  String description;
+  num price;
+  num mrp;
+  String varientImage;
+  String unit;
+  num quantity;
+  Null dealPrice;
+  Null validFrom;
+  Null validTo;
+  String isFavourite;
+  num cartQty;
+  num avgrating;
+  num countrating;
+  num discountper;
+
+  Varients(
+      {this.storeId,
+      this.stock,
+      this.varientId,
+      this.description,
+      this.price,
+      this.mrp,
+      this.varientImage,
+      this.unit,
+      this.quantity,
+      this.dealPrice,
+      this.validFrom,
+      this.validTo,
+      this.isFavourite,
+      this.cartQty,
+      this.avgrating,
+      this.countrating,
+      this.discountper});
+
+  Varients.fromJson(Map<String, dynamic> json) {
+    storeId = json['store_id'];
+    stock = json['stock'];
+    varientId = json['varient_id'];
+    description = json['description'];
+    price = json['price'];
+    mrp = json['mrp'];
+    varientImage = json['varient_image'];
+    unit = json['unit'];
+    quantity = json['quantity'];
+    dealPrice = json['deal_price'];
+    validFrom = json['valid_from'];
+    validTo = json['valid_to'];
+    isFavourite = json['isFavourite'];
+    cartQty = json['cart_qty'];
+    avgrating = json['avgrating'];
+    countrating = json['countrating'];
+    discountper = json['discountper'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['store_id'] = this.storeId;
+    data['stock'] = this.stock;
+    data['varient_id'] = this.varientId;
+    data['description'] = this.description;
+    data['price'] = this.price;
+    data['mrp'] = this.mrp;
+    data['varient_image'] = this.varientImage;
+    data['unit'] = this.unit;
+    data['quantity'] = this.quantity;
+    data['deal_price'] = this.dealPrice;
+    data['valid_from'] = this.validFrom;
+    data['valid_to'] = this.validTo;
+    data['isFavourite'] = this.isFavourite;
+    data['cart_qty'] = this.cartQty;
+    data['avgrating'] = this.avgrating;
+    data['countrating'] = this.countrating;
+    data['discountper'] = this.discountper;
+    return data;
+  }
+}
+
+class Images {
+  String image;
+
+  Images({this.image});
+
+  Images.fromJson(Map<String, dynamic> json) {
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['image'] = this.image;
+    return data;
+  }
+}
+
+class Tags {
+  num tagId;
+  num productId;
+  String tag;
+
+  Tags({this.tagId, this.productId, this.tag});
+
+  Tags.fromJson(Map<String, dynamic> json) {
+    tagId = json['tag_id'];
+    productId = json['product_id'];
+    tag = json['tag'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['tag_id'] = this.tagId;
+    data['product_id'] = this.productId;
+    data['tag'] = this.tag;
+    return data;
+  }
+}
+
+class Topselling {
+  num storeId;
+  num stock;
+  num varientId;
+  num productId;
   String productName;
   String productImage;
   String description;
-  int price;
-  int mrp;
-  Null varientImage;
+  num price;
+  num mrp;
+  String varientImage;
   String unit;
-  int quantity;
+  num quantity;
   String type;
+  num isSubscribe;
+  num count;
   String isFavourite;
-  int cartQty;
-  int avgrating;
-  int countrating;
+  num cartQty;
+  num avgrating;
+  num countrating;
   num discountper;
   List<Varients> varients;
   List<Images> images;
   List<Tags> tags;
 
-  Whatsnew(
+  Topselling(
       {this.storeId,
       this.stock,
       this.varientId,
@@ -563,6 +448,8 @@ class Whatsnew {
       this.unit,
       this.quantity,
       this.type,
+      this.isSubscribe,
+      this.count,
       this.isFavourite,
       this.cartQty,
       this.avgrating,
@@ -572,7 +459,7 @@ class Whatsnew {
       this.images,
       this.tags});
 
-  Whatsnew.fromJson(Map<String, dynamic> json) {
+  Topselling.fromJson(Map<String, dynamic> json) {
     storeId = json['store_id'];
     stock = json['stock'];
     varientId = json['varient_id'];
@@ -582,10 +469,12 @@ class Whatsnew {
     description = json['description'];
     price = json['price'];
     mrp = json['mrp'];
-    // varientImage = json['varient_image'];
+    varientImage = json['varient_image'];
     unit = json['unit'];
     quantity = json['quantity'];
     type = json['type'];
+    isSubscribe = json['is_subscribe'];
+    count = json['count'];
     isFavourite = json['isFavourite'];
     cartQty = json['cart_qty'];
     avgrating = json['avgrating'];
@@ -622,10 +511,134 @@ class Whatsnew {
     data['description'] = this.description;
     data['price'] = this.price;
     data['mrp'] = this.mrp;
-    // data['varient_image'] = this.varientImage;
+    data['varient_image'] = this.varientImage;
     data['unit'] = this.unit;
     data['quantity'] = this.quantity;
     data['type'] = this.type;
+    data['is_subscribe'] = this.isSubscribe;
+    data['count'] = this.count;
+    data['isFavourite'] = this.isFavourite;
+    data['cart_qty'] = this.cartQty;
+    data['avgrating'] = this.avgrating;
+    data['countrating'] = this.countrating;
+    data['discountper'] = this.discountper;
+    if (this.varients != null) {
+      data['varients'] = this.varients.map((v) => v.toJson()).toList();
+    }
+    if (this.images != null) {
+      data['images'] = this.images.map((v) => v.toJson()).toList();
+    }
+    if (this.tags != null) {
+      data['tags'] = this.tags.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Whatsnew {
+  num storeId;
+  num stock;
+  num varientId;
+  num productId;
+  String productName;
+  String productImage;
+  String description;
+  num price;
+  num mrp;
+  Null varientImage;
+  String unit;
+  num quantity;
+  String type;
+  num isSubscribe;
+  String isFavourite;
+  num cartQty;
+  num avgrating;
+  num countrating;
+  num discountper;
+  List<Varients> varients;
+  List<Images> images;
+  List<Tags> tags;
+
+  Whatsnew(
+      {this.storeId,
+      this.stock,
+      this.varientId,
+      this.productId,
+      this.productName,
+      this.productImage,
+      this.description,
+      this.price,
+      this.mrp,
+      this.varientImage,
+      this.unit,
+      this.quantity,
+      this.type,
+      this.isSubscribe,
+      this.isFavourite,
+      this.cartQty,
+      this.avgrating,
+      this.countrating,
+      this.discountper,
+      this.varients,
+      this.images,
+      this.tags});
+
+  Whatsnew.fromJson(Map<String, dynamic> json) {
+    storeId = json['store_id'];
+    stock = json['stock'];
+    varientId = json['varient_id'];
+    productId = json['product_id'];
+    productName = json['product_name'];
+    productImage = json['product_image'];
+    description = json['description'];
+    price = json['price'];
+    mrp = json['mrp'];
+    varientImage = json['varient_image'];
+    unit = json['unit'];
+    quantity = json['quantity'];
+    type = json['type'];
+    isSubscribe = json['is_subscribe'];
+    isFavourite = json['isFavourite'];
+    cartQty = json['cart_qty'];
+    avgrating = json['avgrating'];
+    countrating = json['countrating'];
+    discountper = json['discountper'];
+    if (json['varients'] != null) {
+      varients = new List<Varients>();
+      json['varients'].forEach((v) {
+        varients.add(new Varients.fromJson(v));
+      });
+    }
+    if (json['images'] != null) {
+      images = new List<Images>();
+      json['images'].forEach((v) {
+        images.add(new Images.fromJson(v));
+      });
+    }
+    if (json['tags'] != null) {
+      tags = new List<Tags>();
+      json['tags'].forEach((v) {
+        tags.add(new Tags.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['store_id'] = this.storeId;
+    data['stock'] = this.stock;
+    data['varient_id'] = this.varientId;
+    data['product_id'] = this.productId;
+    data['product_name'] = this.productName;
+    data['product_image'] = this.productImage;
+    data['description'] = this.description;
+    data['price'] = this.price;
+    data['mrp'] = this.mrp;
+    data['varient_image'] = this.varientImage;
+    data['unit'] = this.unit;
+    data['quantity'] = this.quantity;
+    data['type'] = this.type;
+    data['is_subscribe'] = this.isSubscribe;
     data['isFavourite'] = this.isFavourite;
     data['cart_qty'] = this.cartQty;
     data['avgrating'] = this.avgrating;
